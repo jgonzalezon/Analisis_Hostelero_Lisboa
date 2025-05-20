dbf748e49dd7d4fb89ffcc8cbca9e7d010a00d71
Francisco Javier González <61271944+jgonzalezon@users.noreply.github.com>
2025-05-20 17:26:37 +0200

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/Analisis_Hostelero_Lisboa/',    // ← el nombre exacto de tu repositorio
+  // … el resto de tu config si tienes plugins, alias, etc.
+});
